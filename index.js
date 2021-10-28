629b4a1263811e2be471ada7c9f3f193a419d424
Jorge Trad <jetradl@gmail.com>
2021-10-27 23:44:56 -0500

    Routing with Express.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hola, mi server en express');
 });
 
+app.get('/new-route', (req, res) => {
+  res.send('Hola, mi server en express');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'product 1',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
