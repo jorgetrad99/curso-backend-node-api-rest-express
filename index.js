59fcc8d382ea5ca52320b9e2724f3bd21d253fb9
Jorge Trad <jetradl@gmail.com>
2021-10-30 14:43:02 -0500

    GET: query params

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 
 const app = express();
 const port = 3000;
@@ -12,16 +13,23 @@ app.get('/new-route', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 2000,
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+
+  res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
 });
 
 app.get('/products/:id', (req, res) => {
@@ -39,6 +47,18 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
     categoryId,
     productId,
   });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parámetros');
+  }
 });
 
 app.listen(port, () => {
