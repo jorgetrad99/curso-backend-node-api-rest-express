806b001387732acb078447eba2d6c700a9896746
Jorge Trad <jetradl@gmail.com>
2021-11-07 15:03:59 -0600

    Async await and error catching

--- services/products.service.js
+++ services/products.service.js
@@ -19,7 +19,7 @@ class ProductsService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data,
@@ -28,15 +28,20 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    /* return this.products; */
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.produucts);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find((item) => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
@@ -49,7 +54,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
