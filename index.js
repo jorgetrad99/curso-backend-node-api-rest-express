607b96101bc00886ddbc3cdddfaf7daf7f6c7701
Jorge Trad <jetradl@gmail.com>
2021-11-23 18:48:02 -0600

    Deploying

--- index.js
+++ index.js
@@ -9,18 +9,14 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = [
-  'http://localhost:8080',
-  'http://127.0.0.1:5500',
-  'https://myapp.co',
-];
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
