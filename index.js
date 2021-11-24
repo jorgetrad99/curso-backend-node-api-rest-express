c1d30873221d4fe233138cb9bd73fac5375714ea
Jorge Trad <jetradl@gmail.com>
2021-11-23 18:32:59 -0600

    Troubleshoting with cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {
@@ -8,16 +9,32 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = 5500;
 
 app.use(express.json());
 
+const whitelist = [
+  'http://localhost:8080',
+  'http://127.0.0.1:5500',
+  'https://myapp.co',
+];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
 app.get('/', (req, res) => {
-  res.send('Hola, mi server en express');
+  res.send('Hola mi server en express');
 });
 
-app.get('/new-route', (req, res) => {
-  res.send('Hola, mi server en express');
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
 });
 
 routerApi(app);
